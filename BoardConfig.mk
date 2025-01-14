ab38cc9ea5c47eeee178fa661755c0a1b3ea5510
Kai Jones <kvjones0243@gmail.com>
2025-01-13 18:59:56 -0800

    ellis: Define architecture flags
    Change-Id: I74e4fd259a3286c6be0c5488d329e807cbdc299f
    Signed-off-by: Kai Jones <kvjones0243@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,5 +6,13 @@
 
 DEVICE_PATH := device/motorola/ellis
 
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a53
+
 # Inherit the proprietary files
 include vendor/motorola/ellis/BoardConfigVendor.mk
