6a5fe6fb0f70d23313bb1e838d7e4cf27e07a03e
Kai Jones <kvjones0243@gmail.com>
2025-01-13 19:01:25 -0800

    ellis: define Bootloader and Platform flags
    Change-Id: I1e283c9c540ea50a2ef35f4afcf8ce12a5908833
    Signed-off-by: Kai Jones <kvjones0243@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,5 +14,12 @@ TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := generic
 TARGET_CPU_VARIANT_RUNTIME := cortex-a53
 
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := ellis
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6765
+
 # Inherit the proprietary files
 include vendor/motorola/ellis/BoardConfigVendor.mk
