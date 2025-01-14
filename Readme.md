5c23173086823fe4eaed21d099a7e623ad5faab4
Kai Jones <kvjones0243@gmail.com>
2025-01-13 18:55:07 -0800

    ellis: Initial skeleton tree
    Change-Id: I9522a84101d7eb79e9890185e432dd387d00b460
    Signed-off-by: Kai Jones <kvjones0243@gmail.com>

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Device tree for Motorola Ellis
+
+```
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ extract-files.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_lib import (
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/motorola/ellis',
+]
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+    ): lib_fixup_vendor_suffix,
+}
+
+module = ExtractUtilsModule(
+    'ellis',
+    'motorola',
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
