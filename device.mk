4f4e9cc6c410e647bae72ae483e2834cbb7ec9da
Kai Jones <kvjones0243@gmail.com>
2025-01-15 01:46:23 -0800

    ellis: Initial AOSP overlays
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: Ibb171cec89256332c86d93a25ac6c4c549e17a6b

--- device.mk
+++ device.mk
@@ -45,6 +45,13 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/mediatek
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_device.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_device.xml \
